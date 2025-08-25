2a015155eab66c81188e937b83057c09c9b8ff29
dungduongthanhson <dungduongthanhson@gmail.com>
2025-08-25 09:36:26 +0700

    First commit

---
+++ react-vite-starter/src/App.tsx
@@ -0,0 +1,39 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './redux/store'
+import { increment } from './redux/counter/counter.slide'
+
+function App() {
+
+  const count = useSelector((state: RootState) => state.counter)
+  console.log(">>>> ", count)
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <h1>My current count = {count.value}</h1>
+        <div>
+          <button
+            onClick={() => dispatch(
+              increment()
+            )}
+          >Increase +1</button>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
